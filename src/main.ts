68673584677e7f7c183f13b89900662f97bc6def
Eyasu Sisay <eyasusisay6@gmail.com>
2024-05-16 23:27:56 +0300

    test

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log('Server started at http://localhost:3000');
-  await app.listen(3000);
+  const port = process.env.PORT;
+  console.log('port', port);
+  await app.listen(port);
 }
 bootstrap();
